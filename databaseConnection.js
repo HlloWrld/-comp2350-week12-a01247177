1b7c725820f54a88034d1f718f4740c54d5653f5
hllowrld <ira.bcit@protonmail.com>
2021-04-21 16:00:22 -0700

    testing another error

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,7 +2,7 @@ const database = require("mongoose");
 const is_heroku = process.env.IS_HEROKU || false;
 const databaseName = "lab_example"
 
-const herokuURI ="mongodb+srv://theMongoAdmin:Password123@cluster0.4ulcc.mongodb.net/lab_example?retryWrites=true&w=majority"
+const herokuURI ="mongodb+srv://theMongoAdmin:Password123@cluster0.6m56a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const localURI ="mongodb://localhost/lab_example?authSource=admin&retryWrites=true"
 
 	if (is_heroku) {
@@ -17,3 +17,5 @@ module.exports = database;
 		
 
 //mongodb+srv://theMongoAdmin:Password123@cluster0.6m56a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+// mongodb+srv://theMongoAdmin:Password123@cluster0.4ulcc.mongodb.net/lab_example?retryWrites=true&w=majority
