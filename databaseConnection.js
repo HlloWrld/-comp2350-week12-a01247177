9a3a3fab4dc6db8d1aa4271021d9bd9457670abc
hllowrld <ira.bcit@protonmail.com>
2021-04-21 14:11:23 -0700

    testing connection

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,8 +2,8 @@ const database = require("mongoose");
 const is_heroku = process.env.IS_HEROKU || false;
 const databaseName = "lab_example"
 
-const herokuURI ="mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/lab_exmaple?retryWrites=true&w=majority"
-const localURI ="mongodb://localhost/lab_example?authSource=admin&retryWrites=true;"
+const herokuURI ="mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/"+databaseName+"?retryWrites=true&w=majority"
+const localURI ="mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true;"
 
 	if (is_heroku) {
 	database.connect(herokuURI, {useNewUrlParser: true, useUnifiedTopology: true});
